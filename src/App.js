2a3a1fdf31126981a5c45a7eb5fdcd6cc699f34c
THATOMAS <chachumashori@gmail.com>
2021-12-09 09:20:19 +0200

    Added a new data file to the project, containing all the necessary information.
    setup one state variable for my value,setValue .Destructured
    the data array and assigned all of them to the data[value] array index.
    Then i returned a section with all the individual values rendered.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react"
+import information from "./data"
+import {FaAngleDoubleRight} from "react-icons/fa"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = ()=>{
+    const data = information
+    const [value,setValue] = useState(0)
+    
+
+    const { company,dates,duties,title } = data[value] 
+    return <section className="section">
+    <div className="title">
+        <h2>Experience</h2>
+        <div className="underline"></div>
     </div>
-  );
+    <div className="jobs-center">
+        
+        <article className="job-info">
+            
+            <h3>{title}</h3>
+            <h4>{company}</h4>
+            <p className="job-date">{dates}</p>
+            {duties.map((duty,index)=>{
+                return <div key={index} className="job-desc">
+                    <FaAngleDoubleRight className="job-icon">
+
+                    </FaAngleDoubleRight>
+                    <p>{duty}</p>
+                </div>
+            })}
+        </article>
+    </div>
+    
+    </section>
 }
 
 export default App;
